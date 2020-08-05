281c0e518f91d9ee6b7ce974a4004f1165620247
Matt H <moi.c.matt@hotmail.fr>
2020-08-05 11:15:05 +0200

    Feat: add @ngrx/router-store

--- src/app/modules/app-store/app-store.module.ts
+++ src/app/modules/app-store/app-store.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
@@ -19,6 +20,7 @@ import { AppEffects } from './effects/app.effects';
         strictActionImmutability: true,
       }
     }),
+    StoreRouterConnectingModule.forRoot(),
     EffectsModule.forRoot([AppEffects]),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
   ]
