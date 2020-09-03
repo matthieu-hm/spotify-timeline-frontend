522728d1622f5b38b8c3cbf20858ba9e634e80fb
Matt H <moi.c.matt@hotmail.fr>
2020-09-03 23:21:19 +0200

    Clean AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,4 @@ import { CookieService } from 'ngx-cookie-service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'spotify-timeline';
-
-  constructor(
-    private httpClient: HttpClient,
-  ) { }
-
-  public onClick(): void {
-    this.httpClient
-      .get('https://api.spotify.com/v1/me/following', {
-        params: {
-          type: 'artist'
-        }
-      })
-      .subscribe(response => {
-        console.log(response);
-      })
-    ;
-
-    return;
-  }
-
-}
+export class AppComponent {}
